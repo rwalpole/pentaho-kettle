c295cb113be221e78ccda5b68b13812e86da176e
Adam Retter <adam.retter@googlemail.com>
2022-07-01 23:19:38 +0200

    Cleanup and remove code duplicated from RowMeta

--- engine/src/main/java/org/pentaho/di/trans/steps/multimerge/MultiMergeRowMeta.java
+++ engine/src/main/java/org/pentaho/di/trans/steps/multimerge/MultiMergeRowMeta.java
@@ -1,9 +1,36 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2020 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package org.pentaho.di.trans.steps.multimerge;
 
 import org.pentaho.di.core.row.RowMeta;
 import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.core.row.ValueMetaInterface;
 
+/**
+ * Extends {@link RowMeta} to provide an option
+ * to prevent duplicate named columns across input
+ * streams being copied to the output row.
+ */
 public class MultiMergeRowMeta extends RowMeta {
 
     private final boolean preventDuplicateFields;
@@ -12,8 +39,9 @@ public class MultiMergeRowMeta extends RowMeta {
         super();
         this.preventDuplicateFields = preventDuplicateFields;
     }
+
     @Override
-    public void mergeRowMeta(RowMetaInterface r, String originStepName) {
+    public void mergeRowMeta(final RowMetaInterface r, final String originStepName) {
         lock.writeLock().lock();
         try {
             if (preventDuplicateFields) {
@@ -25,16 +53,7 @@ public class MultiMergeRowMeta extends RowMeta {
                     }
                 }
             } else {
-                for (int x = 0; x < r.size(); x++) {
-                    final ValueMetaInterface field = r.getValueMeta(x);
-                    if (searchValueMeta(field.getName()) == null) {
-                        addValueMeta(field); // Not in list yet: add
-                    } else {
-                        // We want to rename the field to Name[2], Name[3], ...
-                        //
-                        addValueMeta(renameValueMetaIfInRow(field, originStepName));
-                    }
-                }
+                super.mergeRowMeta(r, originStepName);
             }
         } finally {
             lock.writeLock().unlock();
